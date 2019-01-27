af4b56f066a141d829509485ca180ec95913c60f
m4lina17 <lmalina17@gmail.com>
2019-01-27 20:42:41 +0100

    Add files via upload

---
+++ scripts/dopisane_inserty.sql
@@ -0,0 +1,22 @@
+REFUND---------------------------
+
+INSERT INTO refund VALUES(refund_id_sequence.nextval,6,PL8249000052062571359447964,'ALBPPLPW','Amadeusz Olszewski',150,'DONE',sysdate);
+INSERT INTO refund VALUES(refund_id_sequence.nextval,8,PL39249000052604104333833616,'ALBPPLPW','Kondrat Zawadzki',100,'IN PROCESS',sysdate);
+INSERT INTO refund VALUES(refund_id_sequence.nextval,9,PL75249000050435134546884789,'ALBPPLPW','Arkadiusz Nowak',50,'IN PROCESS',sysdate);
+INSERT INTO refund VALUES(refund_id_sequence.nextval,10,PL98249000054301586549340394,'ALBPPLPW','Zygfryd Rutkowski',30,'IN PROCESS',sysdate);
+INSERT INTO refund VALUES(refund_id_sequence.nextval,11,PL93249000057040897863152569,'ALBPPLPW','Piotr Piotrowski',40,'IN PROCESS',sysdate);
+INSERT INTO refund VALUES(refund_id_sequence.nextval,12,PL3249000053930429415082393,'ALBPPLPW','Kazimierz Chmielewski',60,'IN PROCESS',sysdate);
+INSERT INTO refund VALUES(refund_id_sequence.nextval,13,PL45249000059253589387218106,'ALBPPLPW','Aleksander Zielinski',250,'IN PROCESS',sysdate);
+INSERT INTO refund VALUES(refund_id_sequence.nextval,16,PL45249000055149317741141696,'ALBPPLPW','Kacper Kaczmarek',20,'IN PROCESS',sysdate);
+INSERT INTO refund VALUES(refund_id_sequence.nextval,17,PL76249000058486459569737397,'ALBPPLPW','Arkadiusz Nowak',80,'IN PROCESS',sysdate);
+
+
+
+COMPLAINT-----------------------
+INSERT INTO complaint VALUES(refund_id_sequence.nextval,'Moja paczka zostala uszkodzona',8,12,sysdate);
+INSERT INTO complaint VALUES(refund_id_sequence.nextval,'Zostalem obrazony przez waszego pracownika',6,13,sysdate);
+INSERT INTO complaint VALUES(refund_id_sequence.nextval,'Kurier uszkodzil moj samochod',8,14,sysdate);
+INSERT INTO complaint VALUES(refund_id_sequence.nextval,'Wasz pracownik ukradl mi wycieraczke',9,1,sysdate);
+INSERT INTO complaint VALUES(refund_id_sequence.nextval,'Ten typ wybil mi szybe',6,15,sysdate);
+INSERT INTO complaint VALUES(refund_id_sequence.nextval,'Wasz pracownik przejechal mi psa',3,7,sysdate);
+INSERT INTO complaint VALUES(refund_id_sequence.nextval,'Ten nikczemnik tak szarpal mi klamke ze urwal',10,2,sysdate);
